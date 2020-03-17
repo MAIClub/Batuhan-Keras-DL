fcc8a45ac8ad9ec71ebaed537d51b3c7ece0e844
batuhanyenidunya <mail@batuhanyenidunya.com>
2020-03-17 19:00:45 +0300

    halo

--- Fer-2013/cnn.py
+++ Fer-2013/cnn.py
@@ -1,6 +1,6 @@
-import pandas as pd 
+import pandas as pd
 import numpy as np
-from keras.models import Sequential 
+from keras.models import Sequential
 from sklearn.model_selection import train_test_split
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.losses import categorical_crossentropy
@@ -12,14 +12,14 @@ from keras.callbacks import ModelCheckpoint
 label_count = 7
 batch_size = 128
 epochs = 100
-width, height = 48 , 48 
+width, height = 48, 48
 
 
 x = np.load('data.npy')
 y = np.load('labels.npy')
 
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.3, random_state = 74)
-X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size = 0.15, random_state = 49)
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=74)
+X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size=0.15, random_state=49)
 
 np.save('X_test.npy', X_test)
 np.save('y_test.npy', y_test)
@@ -28,22 +28,22 @@ np.save('y_test.npy', y_test)
 model = Sequential()
 
 
-model.add(Conv2D(16,kernel_size=(3,3), activation='relu', input_shape=(width, height, 1),kernel_regularizer=l2(0.015)))
-model.add(Conv2D(32, kernel_size=(3,3), activation='relu', padding='same'))
-model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
-model.add(Conv2D(64, kernel_size=(3,3), activation='relu', padding='same'))
+model.add(Conv2D(16, kernel_size=(3, 3), activation='relu', input_shape=(width, height, 1), kernel_regularizer=l2(0.015)))
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', padding='same'))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu', padding='same'))
 model.add(Dropout(0.2))
 
 model.add(Flatten())
 
 
 model.add(Dense(64, activation='tanh'))
-model.add(Dense(2*64, activation='tanh'))
+model.add(Dense(2 * 64, activation='tanh'))
 model.add(Dense(64, activation='tanh'))
 model.add(Dense(label_count, activation='softmax'))
 
 
-model.compile(loss=categorical_crossentropy, optimizer=Adam(lr=0.003),metrics=['accuracy'])
+model.compile(loss=categorical_crossentropy, optimizer=Adam(lr=0.003), metrics=['accuracy'])
 
 model.summary()
 
@@ -51,18 +51,13 @@ filepath = "~/Documents/AI_Related/emotion_recog_fer/savedmodels/Model_1/modelcp
 checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
 callbacks_list = [checkpoint]
 
-trained = model.fit(np.array(X_train), np.array(y_train), 
-                    batch_size=batch_size, 
-                    epochs=epochs, 
-                    verbose=1, 
-                    validation_data=(np.array(X_valid), np.array(y_valid)), 
+trained = model.fit(np.array(X_train), np.array(y_train),
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    verbose=1,
+                    validation_data=(np.array(X_valid), np.array(y_valid)),
                     shuffle=True,
                     callbacks=callbacks_list)
 
 print("****************************************************************************")
 print("train completed")
-
-
-
-
-
--- MaleriaDetectionWithKeras/keras_playground.py
+++ MaleriaDetectionWithKeras/keras_playground.py
@@ -5,16 +5,15 @@ Created on Wed Mar 27 12:45:53 2019
 
 @author: batuhan
 """
-import pandas as pd
 import numpy as np
 import os
 import cv2
 from PIL import Image
-from keras.utils import np_utils
 import keras
-from keras.layers import Conv2D,MaxPooling2D,Dense,Flatten,Dropout
+from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
 from keras.models import Sequential
-#%% Getting Dataset ready and save it for later usage
+from keras.models import load_model
+# Getting Dataset ready and save it for later usage
 data = []
 labels = []
 infected = os.listdir("cell_images/Parasitized/")
@@ -22,9 +21,9 @@ uninfected = os.listdir("cell_images/Uninfected/")
 
 for imageOne in infected:
     try:
-        img = cv2.imread("cell_images/Parasitized/"+imageOne)
+        img = cv2.imread("cell_images/Parasitized/" + imageOne)
         img_from_array = Image.fromarray(img, "RGB")
-        size_image = img_from_array.resize((50,50))
+        size_image = img_from_array.resize((50, 50))
         data.append(np.array(size_image))
         labels.append(0)
     except AttributeError:
@@ -32,120 +31,95 @@ for imageOne in infected:
 
 for imageTwo in uninfected:
     try:
-        img = cv2.imread("cell_images/Uninfected/"+imageTwo)
+        img = cv2.imread("cell_images/Uninfected/" + imageTwo)
         img_from_array = Image.fromarray(img, "RGB")
-        size_image = img_from_array.resize((50,50))
+        size_image = img_from_array.resize((50, 50))
         data.append(np.array(size_image))
         labels.append(1)
     except AttributeError:
         print("Uninfected data loading")
-        
+
 Dataset = np.array(data)
 Labels = np.array(labels)
 
-np.save("50by50Dataset",Dataset)
-np.save("50by50Labels",Labels)
-#%% Loading Dataset and preparing for cnn
+np.save("50by50Dataset", Dataset)
+np.save("50by50Labels", Labels)
+# Loading Dataset and preparing for cnn
 loaded_dataset = np.load("50by50Dataset.npy")
 loaded_labels = np.load("50by50Labels.npy")
 
-s=np.arange(loaded_dataset.shape[0])
+s = np.arange(loaded_dataset.shape[0])
 np.random.shuffle(s)
 loaded_dataset = loaded_dataset[s]
 loaded_labels = loaded_labels[s]
 
-num_classes=len(np.unique(loaded_labels))
-len_data=len(loaded_dataset)
+num_classes = len(np.unique(loaded_labels))
+len_data = len(loaded_dataset)
 
-(x_train,x_test)=loaded_dataset[(int)(0.1*len_data):],loaded_dataset[:(int)(0.1*len_data)]
-x_train = x_train.astype('float32')/255 #Normalize
-x_test = x_test.astype('float32')/255
-train_len=len(x_train)
-test_len=len(x_test)
+(x_train, x_test) = loaded_dataset[(int)(0.1 * len_data):], loaded_dataset[:(int)(0.1 * len_data)]
+x_train = x_train.astype('float32') / 255  # Normalize
+x_test = x_test.astype('float32') / 255
+train_len = len(x_train)
+test_len = len(x_test)
 
-(y_train,y_test)=loaded_labels[(int)(0.1*len_data):],loaded_labels[:(int)(0.1*len_data)]
+(y_train, y_test) = loaded_labels[(int)(0.1 * len_data):], loaded_labels[:(int)(0.1 * len_data)]
 
-y_train=keras.utils.to_categorical(y_train,num_classes)
-y_test=keras.utils.to_categorical(y_test,num_classes)
-#%% Create cnn
-model=Sequential()
-model.add(Conv2D(filters=16,kernel_size=2,padding="same",activation="relu",input_shape=(50,50,3)))
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+# Create cnn
+model = Sequential()
+model.add(Conv2D(filters=16, kernel_size=2, padding="same", activation="relu", input_shape=(50, 50, 3)))
 model.add(MaxPooling2D(pool_size=2))
-model.add(Conv2D(filters=32,kernel_size=2,padding="same",activation="relu"))
+model.add(Conv2D(filters=32, kernel_size=2, padding="same", activation="relu"))
 model.add(MaxPooling2D(pool_size=2))
-model.add(Conv2D(filters=64,kernel_size=2,padding="same",activation="relu"))
+model.add(Conv2D(filters=64, kernel_size=2, padding="same", activation="relu"))
 model.add(MaxPooling2D(pool_size=2))
 model.add(Flatten())
-model.add(Dense(500,activation="relu"))
+model.add(Dense(500, activation="relu"))
 model.add(Dropout(0.2))
-model.add(Dense(2,activation="softmax"))#2 represent output layer neurons 
+model.add(Dense(2, activation="softmax"))  # 2 represent output layer neurons
 model.summary()
-#%% Fit CNN
+# Fit CNN
 
 # compile the model with loss as categorical_crossentropy and using
-#adam optimizer you can test result by trying RMSProp as well as Momentum
+# adam optimizer you can test result by trying RMSProp as well as Momentum
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-#Fit the model with min batch size as 50[can tune batch size to some factor of 2^power ] 
-fitted = model.fit(x_train,y_train,batch_size=50,epochs=20,verbose=1)
+# Fit the model with min batch size as 50[can tune batch size to some factor of 2^power ]
+fitted = model.fit(x_train, y_train, batch_size=50, epochs=20, verbose=1)
 
 accuracy = model.evaluate(x_test, y_test, verbose=1)
 print('\n', 'Test_Accuracy:-', accuracy[1])
 
-
-
-from keras.models import load_model
 model.save('weights_for_50px.h5')
 
-#%% prediction func
+# prediction func
+
+
 def convert_to_array(img):
     im = cv2.imread(img)
     im = cv2.resize(im, (50, 50))
     return np.array(im)
+
+
 def get_cell_name(label):
-    if label==0:
+    if label == 0:
         return "Paracitized"
-    if label==1:
+    if label == 1:
         return "Uninfected"
+
+
 def predict_image(file):
     model = load_model('weights_for_50px.h5')
     print("Predicting Image.................................")
-    ar=convert_to_array(file)
-    ar=ar/255
-    a=[]
+    ar = convert_to_array(file)
+    ar = ar / 255
+    a = []
     a.append(ar)
-    a=np.array(a)
-    score=model.predict(a,verbose=1)
+    a = np.array(a)
+    score = model.predict(a, verbose=1)
     print(score)
-    label_index=np.argmax(score)
+    label_index = np.argmax(score)
     print(label_index)
-    acc=np.max(score)
-    Cell=get_cell_name(label_index)
-    return "The predicted Image is a "+Cell+" with accuracy =  "+str(acc)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    acc = np.max(score)
+    Cell = get_cell_name(label_index)
+    return "The predicted Image is a " + Cell + " with accuracy =  " + str(acc)
